6989b1081b563c5b7105ed12b45e88dc34e2a298
JORDI\Jordi <jordi_martinezjimenez@iescarlesvallbona.cat>
2023-09-26 20:09:25 +0200

    First useState and some Inputs Events

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,13 +1,23 @@
+import {useState, useEffect} from "react";
+
 const Formulario = () => {
+  const [nombre, setNombre] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Enviando formulario...");
+  }
+  
+
   return (
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
       <p className="textl-lg mt-5 text-center mb-10">Añade Pacientes y <span className="text-rose-600 font-bold">Administralos</span></p>
 
-      <form action="" className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
         <div className="mb-5">
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
-          <input id="mascota" type="text" placeholder="Nombre de la mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+          <input id="mascota" type="text" placeholder="Nombre de la mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" value={nombre} onChange={(e) => setNombre(e.target.value)} />
         </div>
         <div className="mb-5">
           <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">Nombre Propietario</label>
@@ -26,7 +36,7 @@ const Formulario = () => {
           <textarea id="sintomas" placeholder="Describe los Síntomas" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
         </div>
 
-        <input type="sumbit" className="bg-rose-600 w-full p-3 text-white text-center uppercase font-bold cursor-pointer hover:bg-rose-700 transition-colors" value="Agregar Paciente" />
+        <button type="sumbit" className="bg-rose-600 w-full p-3 text-white text-center uppercase font-bold cursor-pointer hover:bg-rose-700 transition-colors">Agregar Paciente</button>
       </form>
     </div>
   )
