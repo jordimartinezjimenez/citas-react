4b638f1df3adebb0f61f8dddad4515530e5b51f6
JORDI\Jordi <jordi_martinezjimenez@iescarlesvallbona.cat>
2023-10-01 19:26:15 +0200

    Form Validation and Introduction to Props

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,39 +1,71 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
-const Formulario = () => {
+const Formulario = ({ pacientes, setPacientes }) => {
   const [nombre, setNombre] = useState("");
+  const [propietario, setPropietario] = useState("");
+  const [email, setEmail] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [sintomas, setSintomas] = useState("");
+
+  const [error, setError] = useState(false);
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Enviando formulario...");
+
+    // Validación del Formulario
+    if ([nombre, propietario, email, fecha, sintomas].includes("")) {
+      console.log("Hay min 1 campo vacio");
+
+      setError(true);
+      return;
+    }
+
+    setError(false);
+
+    // Objeto de Paciente
+    const objetoPaciente = {
+      nombre,
+      propietario,
+      email,
+      fecha,
+      sintomas
+    }
+
+    // console.log(objetoPaciente);
+    setPacientes([...pacientes, objetoPaciente]);
+
+    // Reiniciar el form
+    setNombre(''), setPropietario(''), setEmail(''), setFecha(''), setSintomas('')
   }
-  
+
 
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
       <p className="textl-lg mt-5 text-center mb-10">Añade Pacientes y <span className="text-rose-600 font-bold">Administralos</span></p>
 
       <form onSubmit={handleSubmit} className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        {error && (<div className="bg-red-800 text-white p-3 uppercase font-bold mb-3 rounded-md"><p>Todos los campos son obligatorios</p></div>)}
         <div className="mb-5">
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
           <input id="mascota" type="text" placeholder="Nombre de la mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" value={nombre} onChange={(e) => setNombre(e.target.value)} />
         </div>
         <div className="mb-5">
           <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">Nombre Propietario</label>
-          <input id="propietario" type="text" placeholder="Nombre del Propietario" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+          <input id="propietario" type="text" placeholder="Nombre del Propietario" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" value={propietario} onChange={(e) => setPropietario(e.target.value)} />
         </div>
         <div className="mb-5">
           <label htmlFor="email" className="block text-gray-700 uppercase font-bold">Email</label>
-          <input id="email" type="email" placeholder="Email Contacto Propietario" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+          <input id="email" type="email" placeholder="Email Contacto Propietario" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" value={email} onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div className="mb-5">
           <label htmlFor="alta" className="block text-gray-700 uppercase font-bold">Alta</label>
-          <input id="alta" type="date" className="border-2 w-full p-2 mt-2 rounded-md" />
+          <input id="alta" type="date" className="border-2 w-full p-2 mt-2 rounded-md" value={fecha} onChange={(e) => setFecha(e.target.value)} />
         </div>
         <div className="mb-5">
           <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">Síntomas</label>
-          <textarea id="sintomas" placeholder="Describe los Síntomas" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+          <textarea id="sintomas" placeholder="Describe los Síntomas" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" value={sintomas} onChange={(e) => setSintomas(e.target.value)} />
         </div>
 
         <button type="sumbit" className="bg-rose-600 w-full p-3 text-white text-center uppercase font-bold cursor-pointer hover:bg-rose-700 transition-colors">Agregar Paciente</button>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ function Header() {
             <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
                 Seguimiento Pacientes&nbsp;
                 <span className="text-rose-600">Veterinaria</span>
+                
             </h1>
         </>
     )
