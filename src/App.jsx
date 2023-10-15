4c594e584350bcb01532af1a18f0b8420c6ac8ef
JORDI\Jordi <jordi_martinezjimenez@iescarlesvallbona.cat>
2023-10-15 17:59:10 +0200

    LocalStorage implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
-import { useState } from "react"
 
 function App() {
 
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
+
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem("pacientes")) ?? [];
+      setPacientes(pacientesLS);
+    }
+    obtenerLS();
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+  }, [pacientes])
 
   const eliminarPaciente = (id) => {
     const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id);
